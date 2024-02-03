4075a6320c3c64bbd90dda85f35e29d5cdefd7c9
Kayky Santos Da Cunha Couto <134162809+Kayky1812@users.noreply.github.com>
2024-02-03 14:36:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function rand(min,max){
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg,tempo){
+    return new Promise((resolve,reject) => {
+
+        if(typeof msg != 'string') reject("BAD VALUE");
+
+        setTimeout(() => { 
+            resolve(msg);
+        },tempo);
+    });
+ 
+}
+
+
+//then() é usado com resolve
+//catch() é usado com reject
+
+
+esperaAi ("frase 1", (1,3))
+.then(resposta => {
+    console.log(resposta)
+    return esperaAi("Frase 2", rand(1,3))
+})
+.then(resposta=> {
+    console.log(resposta)
+    return esperaAi(2222, rand(1,3))
+})
+.then(resposta => {
+    console.log(resposta)
+})
+.then(() => {
+   console.log("Eu serei o ultimo a aser exibido.") 
+})
+.catch( e => {
+    console.log("ERRO:", e)
+})
